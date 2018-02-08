05a59e9fd7a57b8a4d0996764969701e2f6f9fe2
Jens Eraßmy <jens.erassmy@artcom.de>
2018-02-08 13:08:05 +0100

    Create mobile game prototype client project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Dependencies
+
+* Git Json Api
+* Websocket server
+
+# Development
+
+* `HTTPS=true npm run watch`
+* Open https://localhost:3000/?gitJsonApi=<gitJsonApiUrl>&wsServer=<wsServerUrl>
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import querystring from "querystring"
+import React from "react"
+import ReactDOM from "react-dom"
+
+import App from "./components/app"
+import Server from "./api/serverApi"
+import registerServiceWorker from "./registerServiceWorker"
+import content from "./Content"
+
+const params = querystring.parse(window.location.search.substring(1))
+
+content.loadCmsData(params.gitJsonApi).then(() => {
+  document.title = content.data.game.name
+  ReactDOM.render(
+    <App
+      server = { new Server(params.wsServer) }
+      content = { content } />,
+    document.getElementById("root")
+  )
+})
+
+
+registerServiceWorker()
